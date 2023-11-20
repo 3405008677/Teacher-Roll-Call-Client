30ba5c68b77c82fa79833ae21a9e7fe19daa2cfa
彭于晏 <3405008677@qq.com>
2023-11-20 09:43:55 +0800

    添加top

--- src/layouts/page/main.vue
+++ src/layouts/page/main.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="bg-red h100%" style="padding: 40px 80px">
     <div class="bg-green h100% relative b-rd-10px p-10px">
-      <div>
-        <button class="hvr-grow">123</button>
-      </div>
+      <div> <TopVue /> </div>
       <div
         class="bg-blue h50% w50px absolute left--50px top-50% flex-c items-center color-#CCFFFF"
         style="transform: translateY(-50%); border-radius: 20% 0px 0px 20%"
@@ -14,12 +12,13 @@
         <el-icon class="hvr-wobble-vertical" size="30"><TrendCharts /></el-icon>
       </div>
       <div>
-        <indexVue />
+        <IndexVue />
       </div>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
-  import indexVue from './index.vue'
+  import IndexVue from './index.vue'
+  import TopVue from './top.vue'
 </script>
 <style lang="scss"></style>
---
+++ src/layouts/page/top.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex items-center justify-between">
+    <h1>标题</h1>
+    <div class="flex items-center">
+      <el-input
+        v-model="inputText"
+        class="w-50 m-2 rounded-6px shadowIcon"
+        size="large"
+        placeholder="Please Input"
+        :prefix-icon="Search"
+      />
+      <p class="ml-10px bg-white flex! p-6px rounded-8px shadowIcon hvr-bob">
+        <el-icon><Bell /></el-icon>
+      </p>
+      <p class="ml-10px bg-white flex! p-6px rounded-8px shadowIcon hvr-bob">
+        <el-icon><Warning /></el-icon>
+      </p>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+  import { Search } from '@element-plus/icons-vue'
+  const inputText = ref('')
+</script>
+<style lang="scss"></style>
